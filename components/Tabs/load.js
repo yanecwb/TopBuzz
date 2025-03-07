2a2cd2001085173537114949810b7b2e7719912f
yanecwb <email>
2025-03-07 22:22:28 +0800

    wip

--- components/Tabs/load.js
+++ components/Tabs/load.js
@@ -9,10 +9,10 @@ async function fetchNewsList() {
     //   // "https://api.gamestrial.com/topBuzz_new_list?origin=toutiao_pc&_signature=_02B4Z6wo00f01-9ROewAAIDCMArKoWwjmqvvdT1AAJwcS3u0cjSp3jqACjaQEHIw5VmUbe3RTMjkdJF3jm0BF0mDYUpCuzdvyN6EUDftDB2cqOIP5pQTD22tUKTntU85-FBHLQqY-IufddWi0c"
     // );
     const { articles } = await $.ajax({
-      url: "https://newsapi.org/v2/everything?q=Apple&from=2025-03-06&sortBy=popularity&apiKey=2a8213bad8204df2a0129b22909fb947",
+      url: "https://api.gamestrial.com/topBuzz_new_list",
       method: "GET",
     });
-
+    console.log(articles[0].content);
     return articles;
   } catch (error) {
     console.error("Error:", error);
@@ -31,8 +31,7 @@ function generateNewsContent(newsList) {
       (i) => `
      <div class="swiper-slide">
           <div class="header">
-            <h1>${i.title}</h1>
-            <p>困困商圈 45.6万阅</p>
+            ${i.title}
           </div>
           <img
             src="${i.urlToImage}"
@@ -41,14 +40,13 @@ function generateNewsContent(newsList) {
         </div>`
     )
     .join("");
-  console.log(swiperItems);
 
   swiper.html(swiperItems);
   //Swiper 轮播图
   new Swiper(".swiper", {
     loop: true,
     pagination: { el: ".swiper-pagination", clickable: true },
-    autoplay: { delay: 3000, disableOnInteraction: false },
+    // autoplay: { delay: 3000, disableOnInteraction: false },
   });
   const cards = newsList
     .splice(3, 2)
@@ -57,7 +55,7 @@ function generateNewsContent(newsList) {
       <div class="card">
         <a href="./details.html?id=${new Date().getTime()}">
           <img src="${i.urlToImage}" alt="编辑精选" class="w-full h-[126px]" />
-          <h3>${i.title}</h3>
+          <h3 class='tru-3'>${i.title}</h3>
         </a>
       </div>
     `
@@ -71,24 +69,24 @@ function generateNewsContent(newsList) {
 
       return `
         <div class="list-card w-100vw">
-          <a href="./details.html?id=${new Date().getTime()}">
+          <a href="${i.url}" class='navigator2Detail' target='_blank'>
             ${
-              // hasThreeImages
-              //   ?
-              // ` <div class="list-card-header">${i.Title}</div>
-              //   <div class="flex justify-start items-center !m-[2px]">
-              //     ${i.Image.url_list
-              //       .map((img) =>`<img src="${img.url}" class="object-cover rounded-md w-[110px] h-[76px] mr-[10px]" />`).join("")}
-              //   </div>
-              //   <div class="text-[#999] text-[12px]">
-              //       <span class="flex items-center justify-between">
-              //         12小时前，52评论
-              //       <span class='flex items-center justify-between'>99 <img src="/assets/like.png" width="12" height="12" class="inline-block ml-[2px]" /></span>
-              //     </span>
-              //   </div>`
-              //   : hasOneImage
-              //   ?
-              ` <div class="list-card-header flex justify-between">
+        // hasThreeImages
+        //   ?
+        // ` <div class="list-card-header">${i.Title}</div>
+        //   <div class="flex justify-start items-center !m-[2px]">
+        //     ${i.Image.url_list
+        //       .map((img) =>`<img src="${img.url}" class="object-cover rounded-md w-[110px] h-[76px] mr-[10px]" />`).join("")}
+        //   </div>
+        //   <div class="text-[#999] text-[12px]">
+        //       <span class="flex items-center justify-between">
+        //         12小时前，52评论
+        //       <span class='flex items-center justify-between'>99 <img src="/assets/like.png" width="12" height="12" class="inline-block ml-[2px]" /></span>
+        //     </span>
+        //   </div>`
+        //   : hasOneImage
+        //   ?
+        ` <div class="list-card-header flex justify-between">
                       <div class="flex flex-col justify-between flex-1 mr-[6px]">
                         <div class="tru-2 min-h-[46px]">${i.title}</div>
                         <div class="text-[#999] text-[12px]">
@@ -101,14 +99,14 @@ function generateNewsContent(newsList) {
                       <img src="${i.urlToImage}" class="object-cover rounded-md w-[110px] h-[76px]" />
                     </div>
             `
-              //      :` <div class="list-card-header max-h-[44px] tru-2">${i.Title}</div>
-              //         <div class="text-[#999] text-[12px]">
-              //             <span class="flex items-center justify-between">
-              //               12小时前，52评论
-              //             <span class='flex items-center justify-between'>99 <img src="/assets/like.png" width="12" height="12" class="inline-block ml-[2px]" /></span>
-              //           </span>
-              //         </div>`
-            }
+        //      :` <div class="list-card-header max-h-[44px] tru-2">${i.Title}</div>
+        //         <div class="text-[#999] text-[12px]">
+        //             <span class="flex items-center justify-between">
+        //               12小时前，52评论
+        //             <span class='flex items-center justify-between'>99 <img src="/assets/like.png" width="12" height="12" class="inline-block ml-[2px]" /></span>
+        //           </span>
+        //         </div>`
+        }
           </a>
         </div>
       `;
@@ -120,7 +118,6 @@ function generateNewsContent(newsList) {
 
 // 切换 Tab
 async function switchTab(index) {
-  console.log(" index", index);
   const tabs = document.querySelectorAll(".tabs-nav li");
   const panes = document.querySelectorAll(".tab-pane");
 
@@ -138,6 +135,7 @@ async function switchTab(index) {
       case 0:
         const newsList = await fetchNewsList();
         cache[index] = generateNewsContent(newsList);
+        console.log($('.navigator2Detail'));
         break;
       default:
         cache[
@@ -185,7 +183,6 @@ $(function () {
 
         // 绑定 Tab 切换事件
         document.querySelectorAll(".tabs-nav li").forEach((tab, index) => {
-          console.log(tab);
           tab.addEventListener("click", () => {
             if (tab.classList.contains("active")) {
               refreshTab(index);
